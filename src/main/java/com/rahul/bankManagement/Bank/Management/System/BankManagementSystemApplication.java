df0319455e89298aa9f4efa401baeb399d7fa3ad
Rahul Kushwaha <raahullkushwaha@gmail.com>
2025-11-02 20:42:07 +0530

    Configure application.properties file

---
+++ src/main/java/com/rahul/bankManagement/Bank/Management/System/BankManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.bankManagement.Bank.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankManagementSystemApplication.class, args);
+	}
+
+}
